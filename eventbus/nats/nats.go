ffcbd91049fd8aa01b7c1e50c69d38cd85104f40
mishel <mishu.drk@gmail.com>
2016-12-31 21:31:23 -0600

    feat(eventbus): add support to Nats.io

---
+++ eventbus/nats/nats.go
@@ -0,0 +1,51 @@
+package nats
+
+import (
+	"cqrs"
+	"encoding/json"
+	"strings"
+
+	nats "github.com/nats-io/go-nats"
+)
+
+//Client nats
+type Client struct {
+	Options nats.Options
+}
+
+//NewClient returns the basic client to access to nats
+func NewClient(urls string, useTLS bool) (*Client, error) {
+	opts := nats.DefaultOptions
+	opts.Secure = useTLS
+	opts.Servers = strings.Split(urls, ",")
+
+	for i, s := range opts.Servers {
+		opts.Servers[i] = strings.Trim(s, " ")
+	}
+
+	return &Client{
+		opts,
+	}, nil
+}
+
+//Publish a event
+func (c *Client) Publish(event cqrs.Event, bucket, subset string) error {
+	nc, err := c.Options.Connect()
+	if err != nil {
+		return err
+	}
+
+	defer nc.Close()
+
+	blob, err := json.Marshal(event)
+	if err != nil {
+		return err
+	}
+
+	subj := bucket + "." + subset
+	nc.Publish(subj, blob)
+	nc.Flush()
+
+	err = nc.LastError()
+	return err
+}
