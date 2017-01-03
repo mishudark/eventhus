f954700baca484def8af5013b0dcd2abd513d531
mishel <mishu.drk@gmail.com>
2017-01-02 23:23:13 -0600

    feat(events): if a source is a pointer, convert to its value

--- event.go
+++ event.go
@@ -92,6 +92,11 @@ func (e *EventType) Events() []string {
 //GetTypeName of given struct
 func GetTypeName(source interface{}) (reflect.Type, string) {
 	rawType := reflect.TypeOf(source)
+
+	//source is a pointer, convert to its value
+	if rawType.Kind() == reflect.Ptr {
+		rawType = rawType.Elem()
+	}
 	name := rawType.String()
 	//we need to extract only the name without the package
 	//name currently follows the format `package.StructName`
