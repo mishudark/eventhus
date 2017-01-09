7e6a6d6d9d806f6687faf4eaa0772734416fca50
mishel <mishu.drk@gmail.com>
2017-01-09 10:48:14 -0600

    fix(event): add json tag

--- event.go
+++ event.go
@@ -14,12 +14,12 @@ var (
 
 //Event stores the data for every event
 type Event struct {
-	ID            string
-	AggregateID   string
-	AggregateType string
-	Version       int
-	Type          string
-	Data          interface{}
+	ID            string      `json:"id"`
+	AggregateID   string      `json:"aggregate_id"`
+	AggregateType string      `json:"aggregate_type"`
+	Version       int         `json:"version"`
+	Type          string      `json:"type"`
+	Data          interface{} `json:"data"`
 }
 
 //Register defines generic methods to create a registry
