55daf22d06da2d922471f72cebed4d1dea7b4a37
mishudark <mishu.drk@gmail.com>
2019-01-25 10:19:26 +0100

    feat(alert): add additional fields

--- v2/alert.go
+++ v2/alert.go
@@ -1,5 +1,7 @@
 package eventhus
 
+import "fmt"
+
 // AlertType defines the alert(error) type while a command is being processed
 type AlertType string
 
@@ -16,8 +18,32 @@ const (
 type Alert struct {
 	CommandID      string    `json:"command_id"`
 	CommandType    string    `json:"command_type"`
-	CommandVersion string    `json:"command_version"`
+	CommandVersion int       `json:"command_version"`
 	AggregateID    string    `json:"aggregate_id"`
 	AggregateType  string    `json:"aggregate_type"`
 	Type           AlertType `json:"type"`
 }
+
+// NewAlert returns an alert that implements an error interface
+func NewAlert(typ AlertType, command Command) Alert {
+	return Alert{
+		CommandID:      command.GetID(),
+		CommandType:    command.GetType(),
+		CommandVersion: command.GetVersion(),
+		AggregateID:    command.GetAggregateID(),
+		AggregateType:  command.GetAggregateType(),
+		Type:           typ,
+	}
+}
+
+func (a *Alert) Error() string {
+	return fmt.Sprintf("[%s]: command-id=%s command-type=%s command-version=%d aggregate-id:%s aggregate_type=%s",
+		a.Type,
+		a.CommandID,
+		a.CommandType,
+		a.CommandVersion,
+		a.AggregateID,
+		a.AggregateType)
+}
+
+var _ error = (*Alert)(nil)
