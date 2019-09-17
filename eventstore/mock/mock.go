f7271d1385d0be2ca6820d68de225732659621ca
FabianTe <fabiante@users.noreply.github.com>
2019-09-17 20:36:03 +0200

    Implement SafeSave (same code as Save)

--- eventstore/mock/mock.go
+++ eventstore/mock/mock.go
@@ -34,7 +34,12 @@ func (c *Client) Save(events []eventhus.Event, version int) error {
 }
 
 func (c *Client) SafeSave(events []eventhus.Event, version int) error {
-	panic("SafeSave is not yet implemented on the mocked event store!")
+	for _, event := range events {
+		aggregateId := event.AggregateID
+		c.events[aggregateId] = append(c.events[aggregateId], event)
+	}
+
+	return nil
 }
 
 func (c *Client) Load(aggregateID string) ([]eventhus.Event, error) {
